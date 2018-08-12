21d0df8a030c950fed723139e8a435524564f197
BlackBeltPanda <dev@pandacraft.org>
2018-08-12 16:47:57 -0400

    Fix toggling MoreFish back on

--- MoreFishToggler/src/org/pandacraft/MoreFishToggler/ToggleCommand.java
+++ MoreFishToggler/src/org/pandacraft/MoreFishToggler/ToggleCommand.java
@@ -6,24 +6,36 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.pandacraft.MoreFishToggler.Utils.SimpleConfig;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class ToggleCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
+			
+			Player player = (Player) sender;
 
-			String uuid = ((Player) sender).getUniqueId().toString();
+			String uuid = player.getUniqueId().toString();
 
 			SimpleConfig playerData = MoreFishToggler.getInstance().getPlayerData();
 
 			if (playerData.contains(uuid)) {
 				playerData.removeKey(uuid);
+				if (MoreFishToggler.getInstance().getPlayers().contains(uuid)) {
+					MoreFishToggler.getInstance().getPlayers().remove(uuid);
+				}
+				player.sendMessage(ChatColor.AQUA + "[MFT]" + ChatColor.GOLD + " Toggled MoreFish" + ChatColor.GREEN + " On.");
+				return true;
 			}
 				
 			playerData.createSection(uuid);;
 			playerData.saveConfig();
 
 			MoreFishToggler.getInstance().getPlayers().add(uuid);
+			
+			player.sendMessage(ChatColor.AQUA + "[MFT]" + ChatColor.GOLD + " Toggled MoreFish" + ChatColor.RED + " Off.");
+			return true;
 		}
 		return false;
 	}
